b6f5aeae67f966d4e3393fff3d9e258bc296a4f1
admin <admin@DESKTOP-6FQJOCG>
2020-02-10 21:13:32 -0800

    Reverse String Done.

---
+++ EXAM/src/xyz/RevString.java
@@ -0,0 +1,28 @@
+package xyz;
+public class RevString {
+	public void reverseWordInMyString(String str)
+	   {
+		
+		String[] words = str.split(" ");
+		String reversedString = "";
+		for (int i = 0; i < words.length; i++)
+	        {
+	           String word = words[i]; 
+	           String reverseWord = "";
+	           for (int j = word.length()-1; j >= 0; j--) 
+		   {
+			
+			reverseWord = reverseWord + word.charAt(j);
+		   }
+		   reversedString = reversedString + reverseWord + " ";
+		}
+		System.out.println(str);
+		System.out.println(reversedString);
+	   }
+	   public static void main(String[] args) 
+	   {
+		RevString obj = new RevString();
+		obj.reverseWordInMyString("Hello Muskan");
+		obj.reverseWordInMyString("Java Training");
+	   }
+}
