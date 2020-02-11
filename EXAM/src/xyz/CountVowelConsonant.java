861a425a0317195e713709dafb4a9ce3d92283bd
admin <admin@DESKTOP-6FQJOCG>
2020-02-10 21:24:20 -0800

    CountVowelConsonant Done.

---
+++ EXAM/src/xyz/CountVowelConsonant.java
@@ -0,0 +1,15 @@
+package xyz;
+
+public class CountVowelConsonant {
+	  public static void main(String[] args) {
+	        String str = "HelloMuskan";
+	        int vcount = 0, ccount = 0;
+	        str = str.toLowerCase();
+	        for(int i = 0; i < str.length(); i++) { char ch = str.charAt(i); if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') { vcount++; } else if((ch >= 'a'&& ch <= 'z')) {
+	                ccount++;
+	            }
+	        }
+	        System.out.println("Number of Vowels: " + vcount);
+	        System.out.println("Number of Consonants: " + ccount);
+	    }
+}
