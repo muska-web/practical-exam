fe4357902e11adf5a599de475f9aa24ff1b23003
admin <admin@DESKTOP-6FQJOCG>
2020-02-10 21:20:17 -0800

    Sum of Array Done.

---
+++ EXAM/src/xyz/SumArray.java
@@ -0,0 +1,11 @@
+package xyz;
+public class SumArray {
+	 public static void main(String args[]){
+	      int[] array = {10, 20, 30, 40, 50, 10};
+	      int sum = 0;
+	      for( int num : array) {
+	          sum = sum+num;
+	      }
+	      System.out.println("Sum of array elements is:"+sum);
+	   }
+}
